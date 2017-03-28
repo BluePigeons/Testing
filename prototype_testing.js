00552516bdd53fc6eadb67546b94dd85192c5975
silentgraphic <kierannolan1995@gmail.com>
2017-03-28 14:58:31 +0100

    Changed the filter staments to their own method
    The filter statemnts are not running correctly and are only returning
    true.Also the else if statments are not returning at all

--- prototype_testing.js
+++ prototype_testing.js
@@ -4,7 +4,7 @@ function Car (doors,colour){
     return colour.charAt(0).toUpperCase() + colour.slice(1);
   };
   this.manufacturer_type=["Ford","Fiat","Saab"];
-  this.available_colours=["Black","Red","Green"]
+  this.available_colours=["Black","Red","Green"];
 };
 
 Car.prototype.write = function(x){
@@ -12,26 +12,35 @@ Car.prototype.write = function(x){
   console.log(x);
 };
 
-Car.prototype.look_up = function() {
+Car.prototype.error_check= function(){
   //checking to see if the .doors is correct
   if (isNaN(this.doors)&&this.doors<4){
-    console.log("That is not an available type");
-    return this
-  };
+    return false;
+  }
   //checking to see if the .colour is correct
-  if (this.available_colours.indexOf(this.colour) >= 0){
-    console.log("That is not an available type");
-    return this;
-  };
-  //Compare and log
-  if (this.doors==4 && this.colour() == "Black") {
-    this.write(0)
-  } else if (this.doors==4 && this.colour() == "Red") {
-    this.write(1)
-  } else if (this.doors==4 && this.colour() == "Green") {
-    this.write(2)
+  else if (this.available_colours.indexOf(this.colour) >= 0){
+    return false;
+  } else {
+    return true;
   };
 };
 
-var my_Car= new Car(4,"red");
+Car.prototype.look_up = function() {
+  //check if this.check returns true or false
+  if(!this.error_check()){
+    throw new error("That is not an available type");
+  }
+  //Compare and log
+  else{
+    if (this.doors==4 && this.colour() == "Black") {
+      this.write(0)
+    } else if (this.doors==4 && this.colour() == "Red") {
+      this.write(1)
+    } else if (this.doors==4 && this.colour() == "Green") {
+      this.write(2)
+    };
+  };
+};
+
+var my_Car= new Car(4,"Blue");
 my_Car.look_up();
